632104a05a5a5277a7fa3179b5826c12ab284d3d
Nazar Koval <n.kovaldev@gmail.com>
2023-06-24 04:54:55 +0300

    update auxiliary info

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+		SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
+	},
+   images: {
+      domains: ["cdn.shopify.com"]
+   }
+}
 
 module.exports = nextConfig
