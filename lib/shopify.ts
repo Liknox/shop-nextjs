ab95f0a6a68d174fe91cf79e585a92d019b3a144
Nazar Koval <n.kovaldev@gmail.com>
2023-06-25 06:38:32 +0300

    feat: create new component and correct types in shopify.ts

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -1,8 +1,7 @@
 const domain = process.env.SHOPIFY_STORE_DOMAIN
 const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
 
-
-async function ShopifyData(query) {
+async function ShopifyData(query: string) {
 	const URL = `https://${domain}/api/2023-04/graphql.json`
 
 	const options = {
@@ -10,14 +9,14 @@ async function ShopifyData(query) {
 		method: "POST",
 		headers: {
 			"X-Shopify-Storefront-Access-Token": storefrontAccessToken,
-			"Accept": "application/json",
+			Accept: "application/json",
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({ query }),
 	}
 
 	try {
-		const data = await fetch(URL, options).then(res => res.json())
+		const data = await fetch(URL, options as RequestInit).then(res => res.json())
 		return data
 	} catch (err) {
 		throw new Error("Products not fetched")
@@ -28,30 +27,30 @@ export async function getProductsInCollection() {
 	const query = `
    {
       collectionByHandle (handle: "frontpage") {
-        title
-        products(first: 25) {
-          edges {
-            node {
-              id
-              title
-              handle
-              images(first: 5) {
-                edges {
-                  node {
-                    url
-                    altText
+         title
+         products(first: 25) {
+            edges {
+               node {
+                  id
+                  title
+                  handle
+                  images(first: 5) {
+                     edges {
+                        node {
+                           url
+                           altText
+                        }
+                     }
                   }
-                }
-              }
+               }
             }
-          }
-        }
+         }
       }
-    }
+   }
    `
 
 	const response = await ShopifyData(query)
 	const allProducts = response.data.collectionByHandle.products.edges ? response.data.collectionByHandle.products.edges : []
 
-   return allProducts
+	return allProducts
 }
