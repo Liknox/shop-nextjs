4679f7f4721deb2f439ad73367bec8faca56f9f6
Nazar Koval <n.kovaldev@gmail.com>
2023-06-25 08:08:25 +0300

    feat: make ProductCard markup
    * create product list output

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,24 @@
-function ProductCard() {
-   return <div>ProductCard</div>
+import Link from "next/link"
+import Image from "next/image"
+
+function ProductCard({ product }: any) {
+	const { title, handle } = product.node
+
+	const { altText, url } = product.node.images.edges[0].node
+
+	return (
+		<div>
+			<Link href={`/product/${handle}`}>
+				<div className="group">
+					<div className="w-full bg-gray-200 rounded-3xl overflow-hidden">
+						<div className="relative group-hover:opacity-75 h-72">
+							<Image src={url} alt={altText} fill style={{objectFit:"cover"}} />
+						</div>
+					</div>
+				</div>
+			</Link>
+		</div>
+	)
 }
 
 export default ProductCard
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,9 +1,15 @@
+import ProductCard from "./ProductCard"
+
 function ProductList({ products }: { products: any[] }) {
 	return (
 		<div className="bg-white">
 			<div className="max-w-2xl mx-auto py-16 px-4">
 				<h2 className="text-2xl font-extrabold text-gray-900 mb-6">Products</h2>
-				<div className="grid grid-cols-1 gap-y-10 gap-x-6">{products.map(product => product.node.handle)}</div>
+				<div className="grid grid-cols-1 gap-y-10 gap-x-6">
+					{products.map(product => (
+						<ProductCard key={product.node.id} product={product} />
+					))}
+				</div>
 			</div>
 		</div>
 	)
