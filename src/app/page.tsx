654a63c14c5abcad074bd089f881da80fada8b5e
Nazar Koval <n.kovaldev@gmail.com>
2023-06-30 10:47:21 +0300

    feat: create big Hero component for Home page
    * adjust height
    * leave buttons blank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ProductList from "../components/ProductList"
 import { getProductsInCollection } from "../../lib/shopify"
+import Hero from "@/components/Hero"
 
 export default async function Home() {
 	const products = await getProductsInCollection()
 	return (
-		<div className="text-lg">
+		<div className="">
+			<Hero />
 			<ProductList products={products} />
 		</div>
 	)
